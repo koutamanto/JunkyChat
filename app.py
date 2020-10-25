6bb066249c1d59b3cf875f9a94c7d34f60f5ea36
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-10-25 09:03:58 +0900

    hey

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def index():
 
 @app.route("/view")
 def view():
-    with open("/static/data.json","r") as f:
+    with open("data.json","r") as f:
         view_data = json.load(f)
     print(view_data)
     return view_data
@@ -20,7 +20,7 @@ def view():
 def send():
     sended_data = request.get_data()
     print(sended_data)
-    with open("/static/data.json","w") as f:
+    with open("data.json","w") as f:
         json.dump(sended_data, f, indent=4)
 
 if __name__ == "__main__":
