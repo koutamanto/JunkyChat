a9b9656ae412330847d026d6d2bdee836a1a6722
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-10-25 09:00:54 +0900

    hey

--- app.py
+++ app.py
@@ -10,5 +10,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/view")
+def view():
+    with open("/static/data.json","r") as f:
+        view_data = json.load(f)
+    print(view_data)
+    return view_data
+@app.route("/send",method=["POST"])
+def send():
+    sended_data = request.get_data()
+    print(sended_data)
+    with open("/static/data.json","w") as f:
+        json.dump(sended_data, f, indent=4)
+
 if __name__ == "__main__":
     app.run(debug=True)
