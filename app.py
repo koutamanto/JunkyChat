d808645f7944771996509bc72510cefb6f64e8ac
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-10-27 20:09:15 +0900

    add creating talkrooms functions #2

--- app.py
+++ app.py
@@ -37,8 +37,8 @@ def view():
 
 @app.route("/send",methods=["POST"])
 def send():
-    msg = json.loads(request.get_data().decode())["msg"]
-    roomname = json.loads(request.get_data().decode())["roomname"]
+    msg = json.loads(request.get_data().decode().replace('\0', ''))["msg"]
+    roomname = json.loads(request.get_data().decode().replace('\0', ''))["roomname"]
     datas_list.append(msg)
     datas[roomname]["datas"] = datas_list
     print(datas)
